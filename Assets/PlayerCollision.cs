cd763517d19f70713fd112a77361c8d3c161307c
DanyaalA <Danny13653@gmail.com>
2021-01-17 23:10:46 +0000

    Cube Run

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement pMovement;
+    public TextMeshProUGUI tmg;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            pMovement.enabled = false;
+            tmg.enabled = true;
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello Maote");
+
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Add a Forward Force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
